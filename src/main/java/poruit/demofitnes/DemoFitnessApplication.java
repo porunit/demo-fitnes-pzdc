c6a0c2e52f37c785f8fc2dd437acff4c9fc232e5
Armemius <rusgy@yandex.ru>
2025-06-07 16:06:08 +0300

    chores: update gitignore
    Exclude env-file from git versioning

--- src/main/java/poruit/demofitnes/DemoFitnessApplication.java
+++ src/main/java/poruit/demofitnes/DemoFitnessApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoitnesApplication {
+public class DemoFitnessApplication {
+    // I am very good programmer and probably able not to write fucked up code in repository consisting of 4 files :3
     public static void main(String[] args) {
-        SpringApplication.run(DemoitnesApplication.class, args);
+        SpringApplication.run(DemoFitnessApplication.class, args);
     }
-
 }
